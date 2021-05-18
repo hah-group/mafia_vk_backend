4db780229214cf09e5f39e8bff8c9afe5b4f318f
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-19 01:12:08 +0300

    Application signature verification has been released

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -7,6 +8,7 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
+      imports: [ConfigModule.forRoot()],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +16,23 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should pass signature verification', () => {
+    expect(
+      service.validateSign(
+        'vk_access_token_settings=&vk_app_id=7477902&vk_are_notifications_enabled=0&vk_is_app_user=0&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_ts=1621375582&vk_user_id=152879324',
+        'bsW9oT606gRZMgNJgY-kzHYiEEmoNnAsTNbqYyu5rxk',
+      ),
+    ).toBeTruthy();
+  });
+
+  it('should fail the test', () => {
+    expect(
+      service.validateSign(
+        'vk_access_token_settings=&vk_app_id=7477900&vk_are_notifications_enabled=0&vk_is_app_user=0&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_ts=1621375582&vk_user_id=152879324&',
+        'bsW9oT606gRZMgNJgY-kzHYiEEmoNnAsTNbqYyu5rxk',
+      ),
+    ).toBeFalsy();
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateSign(params: string, sign: string): boolean {
+    const paramsHash = crypto
+      .createHmac('sha256', process.env.APP_SECRET_KEY)
+      .update(params)
+      .digest()
+      .toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=$/, '');
+
+    return paramsHash === sign;
+  }
+}

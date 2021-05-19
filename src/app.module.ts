37919282e67e703e4bc0c9720fc5da9bc6d0d2a5
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-19 19:34:48 +0300

    Modules connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { VkIoModule } from './vk-io/vk-io.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot(),
     AuthModule,
     VkIoModule.registerAsync({
       token: process.env.API_SERVICE_TOKEN,
     }),
+    PrismaModule,
+    UserModule,
   ],
 })
 export class AppModule {}

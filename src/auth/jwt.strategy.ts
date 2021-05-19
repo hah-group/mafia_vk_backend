ff2d0f163ee07cba2f1ed5bf063a1030d67d16c0
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-19 19:33:30 +0300

    Implemented jwt token verification strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.APP_SECRET_KEY,
+    });
+  }
+
+  async validate(payload: any) {
+    return await this.userService.user({
+      where: {
+        id: payload.sub,
+      },
+    });
+  }
+}

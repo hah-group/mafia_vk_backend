328c127222ba5a03993e22d0ac0227f99637f378
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-19 19:28:41 +0300

    Fixed VkIoModule accessibility scope

--- src/vk-io/vk-io.module.ts
+++ src/vk-io/vk-io.module.ts
@@ -1,8 +1,9 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { IAPIOptions } from 'vk-io/lib/api/api';
 import { VkIoService } from './vk-io.service';
 import { VK_IO_OPTION } from './constants';
 
+@Global()
 @Module({})
 export class VkIoModule {
   static registerAsync(options: Partial<IAPIOptions>): DynamicModule {

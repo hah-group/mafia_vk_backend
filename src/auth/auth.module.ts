3241667fef86e8ca18b1802dc308ad0aaf536f55
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-20 02:31:47 +0300

    Fix jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   imports: [
     UserModule,

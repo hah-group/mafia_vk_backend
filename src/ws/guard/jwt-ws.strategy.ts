c10d585d884ae52d57ede9b251f3b6158b86774b
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-21 23:12:40 +0300

    Reorganizing files

--- src/ws/guard/jwt-ws.strategy.ts
+++ src/ws/guard/jwt-ws.strategy.ts
@@ -1,7 +1,7 @@
 import { Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { UserService } from '../user/user.service';
+import { UserService } from '../../user/user.service';
 import { Socket } from 'socket.io';
 
 @Injectable()

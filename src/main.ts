230f00966fe442487c27984c48f066d0ff979d75
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-25 00:31:24 +0300

    Updated socket.io to the latest version - v4

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SocketIoAdapter } from './socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+  app.useWebSocketAdapter(new SocketIoAdapter(app, [/\w*/]));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }

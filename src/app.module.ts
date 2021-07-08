682111a82989fa3734ca0e8f6bd87a89754650e0
Efim Belyakov <belyakov.efim@gmail.com>
2021-07-08 23:18:05 +0300

    MAF-8 connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Ehfjuehtxbr20',
+      database: 'mafia_app',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

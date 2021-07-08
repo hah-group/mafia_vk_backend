bac4f81ea2c0040866524bf531c09c10fa435a53
Efim Belyakov <belyakov.efim@gmail.com>
2021-07-08 23:59:13 +0300

    MAF-8 delete db depends

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Ehfjuehtxbr20',
-      database: 'mafia_app',
-      models: [],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

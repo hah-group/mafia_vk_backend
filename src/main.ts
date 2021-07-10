97f40cca9a52b7fd2c5d81adf914d89e5c872d3f
Efim Belyakov <belyakov.efim@gmail.com>
2021-07-10 11:41:19 +0300

    MAF-13 add package env scripts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.MAF_SERVER_PORT);
+  Logger.log(
+    `The server is running on port ${process.env.MAF_SERVER_PORT}`,
+    'NestApplication',
+    false,
+  );
 }
 
 bootstrap();

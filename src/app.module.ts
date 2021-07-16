21b49732939fb3834c287e5d456fa8f2bd13b197
Efim Belyakov <belyakov.efim@gmail.com>
2021-07-16 19:57:15 +0300

    MAF-22 first timers structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: process.env.MAF_REDIS_HOST,
+        port: parseInt(process.env.MAF_REDIS_PORT),
+      },
+      defaultJobOptions: {
+        removeOnComplete: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
